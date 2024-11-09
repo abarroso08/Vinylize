787e1443279a5a17b707ad146d8931bfc4a4233f
Javier <javier.fefefe@gmail.com>
2024-11-09 15:05:30 -0500

    Added tab icon

--- assets/head.php
+++ assets/head.php
@@ -4,7 +4,7 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+     <link rel="icon" type="image/x-icon" href="/images/favicon_io/favicon.ico">
      <title>Vinylize</title>
      <!-- Bootstrap CSS -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
